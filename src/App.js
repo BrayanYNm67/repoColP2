b66ec6261de38ff09d3e3fb67116a0fce6a842cb
BrayanYNm67 <fnafbray67@gmail.com>
2025-06-11 15:06:19 -0600

    Pagina de inicio modificada

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleClick = () => {
+    alert('¡Gracias por visitar nuestra app!');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>¡Bienvenido a Mi Aplicación React!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Esta es mi segunda pagina en React <code></code> 
         </p>
+        <button onClick={handleClick} className="App-button">
+          Haz clic aquí
+        </button>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://es.react.dev/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende React (documentación en español)
         </a>
       </header>
     </div>
