8a5e82d40835e2360a452f87c0dce39b182a8bf9
BrayanYNm67 <fnafbray67@gmail.com>
2025-06-12 19:44:46 -0600

    Corregido

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './layout';
+//import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//import Layout from './layout';
 import logo from './logo.svg';
 import './App.css';
 
